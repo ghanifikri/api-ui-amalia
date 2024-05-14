06f8cf770697ae330aac30a9798f21cf8366f99d
ghanifikri <ghanifikribaihaqi@gmail.com>
2024-05-14 08:21:10 +0700

    Install Jetstream

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('role_id')->default(1);
+            $table->string('azure_id')->unique()->nullable();
+            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
