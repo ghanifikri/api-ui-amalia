a87af90354762deef5e09f1b89448f921d7742a3
ghanifikri <ghanifikribaihaqi@gmail.com>
2024-05-14 08:21:32 +0700

    first-commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('role_id')->default(1);
-            $table->string('azure_id')->unique()->nullable();
-            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
